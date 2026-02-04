fc4dbead74f668bbd54639184a56410dbb792909
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-02-04 12:31:11 +0100

    test case for Option<T>

---
+++ toml-pretty-deser/tests/option.rs
@@ -0,0 +1,58 @@
+use std::{cell::RefCell, rc::Rc};
+use toml_pretty_deser::{
+    AnnotatedError, AsMap, AsMapEnum, AsMapNested, AsMapTaggedEnum, AsMapVec, AsMapVecEnum,
+    AsMapVecNested, AsMapVecTaggedEnum, DeserError, FromTomlItem, FromTomlTable, StringNamedEnum,
+    ToConcrete, TomlHelper, TomlValue, VerifyFromToml, deserialize, make_partial,
+    make_partial_enum,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct DNA(String);
+
+impl PartialEq<&str> for DNA {
+    fn eq(&self, other: &&str) -> bool {
+        &self.0 == *other
+    }
+}
+
+#[make_partial]
+#[derive(Debug)]
+struct BarcodesValidated {
+    barcode: Option<DNA>,
+}
+
+impl FromTomlItem for DNA {
+    fn from_toml_item(
+        item: &toml_edit::Item,
+        parent_span: std::ops::Range<usize>,
+    ) -> TomlValue<DNA> {
+        match item.as_str() {
+            Some(s) => {
+                if s.chars()
+                    .all(|c| matches!(c, 'a' | 'c' | 'g' | 't' | 'A' | 'C' | 'G' | 'T'))
+                {
+                    TomlValue::new_ok(DNA(s.to_string()), parent_span)
+                } else {
+                    TomlValue::new_validation_failed(
+                        item.span().unwrap_or(parent_span),
+                        "Invalid base".to_string(),
+                        Some("Use only AGTC".to_string()),
+                    )
+                }
+            }
+            None => TomlValue::new_wrong_type(item, parent_span, "String(DNA)"),
+        }
+    }
+}
+
+#[test]
+fn test_opt_validated() {
+    let toml = "
+        barcodes = 'agtc'
+    ";
+    let result: Result<_, _> = deserialize::<PartialBarcodesValidated, BarcodesValidated>(toml);
+    assert!(result.is_ok());
+    if let Ok(output) = result {
+        assert_eq!(output.barcode, Some(DNA("agtc".to_string())))
+    }
+}
