02d6bb402fe43ccd9b5d198bc26e0e47c20eab91
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-02-06 15:01:25 +0100

    skip test case

---
+++ toml-pretty-deser/tests/skip.rs
@@ -0,0 +1,21 @@
+use toml_pretty_deser::prelude::*;
+
+#[tpd]
+#[derive(Debug)]
+struct Skipped {
+    a_u8: u8,
+    #[tpd_skip]
+    skipped: u8,
+}
+
+#[test]
+fn test_skipped() {
+    let toml = "a_u8 = 123";
+    let result: Result<_, _> = deserialize::<PartialSkipped, Skipped>(toml);
+    dbg!(&result);
+    assert!(result.is_ok());
+    if let Ok(output) = result {
+        assert_eq!(output.a_u8, 123);
+        assert_eq!(output.skipped, Default::default());
+    }
+}
